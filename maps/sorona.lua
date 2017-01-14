2de1457c6325d5f5418472a88401169a2f7368ed
MilkingChicken <THECHEESEMEISTERS@GMAIL.COM>
2017-01-14 22:40:21 +1000

    Sorona V1

--- maps/sorona.lua
+++ maps/sorona.lua
@@ -15,32 +15,38 @@ return {
 		{x =  15, y = -80}
 	},
 	-- GRAPHICS
-	clouds = true,
-	background = "assets/backgrounds/default.png",
+	clouds = false,
+	background = "assets/backgrounds/sorona.png",
 	platforms = {
 		{
 			x = -91,
 			y = 0,
 			shape = {0,1, 181,1, 181,10, 96,76, 86,76, 0,10},
-			sprite = "assets/platforms/default-big.png"
+			sprite = "assets/platforms/sorona-center.png"
 		},
 		{
 			x = 114,
 			y = 50,
 			shape = {0,1, 52,1, 52,30, 0,30},
-			sprite = "assets/platforms/default-side.png"
+			sprite = "assets/platforms/sorona-left-bottom.png"
 		},
 		{
 			x = -166,
 			y = 50,
 			shape = {0,1, 52,1, 52,30, 0,30},
-			sprite = "assets/platforms/default-side.png"
+			sprite = "assets/platforms/sorona-right-bottom.png"
 		},
 		{
-			x = -17,
+			x = 114,
 			y = -50,
 			shape = {0,1, 34,1, 34,16, 0,16},
-			sprite = "assets/platforms/default-top.png"
+			sprite = "assets/platforms/sorona-left-top.png"
+		},
+		{
+			x = -166,
+			y = -50,
+			shape = {0,1, 34,1, 34,16, 0,16},
+			sprite = "assets/platforms/sorona-right-top.png"
 		}
 	},
 	decorations = {}
