75644b81561cc42d80d3a5fb3256974c911979a9
MilkingChicken <THECHEESEMEISTERS@GMAIL.COM>
2017-01-16 01:30:43 +1000

    Establishing alpha.lua.

---
+++ maps/alpha.lua
@@ -0,0 +1,47 @@
+-- The abyss of the alpha.
+return {
+	-- GENERAL
+	name = "alpha",
+	theme = "alpha.ogg",
+	center_x = 0,
+	center_y = 0,
+	width  = 360,
+	height = 240,
+	-- RESPAWN POINTS
+	respawns = {
+		{x = -15, y = -80},
+		{x =  -5, y = -80},
+		{x =   5, y = -80},
+		{x =  15, y = -80}
+	},
+	-- GRAPHICS
+	clouds = true,
+	background = "assets/backgrounds/alpha.png",
+	platforms = {
+		{
+			x = -91,
+			y = 0,
+			shape = {0,1, 181,1, 181,10, 96,76, 86,76, 0,10},
+			sprite = "assets/platforms/alpha-big-1.png"
+		},
+		{
+			x = 114,
+			y = 50,
+			shape = {0,1, 52,1, 52,30, 0,30},
+			sprite = "assets/platforms/alpha-small-1.png"
+		},
+		{
+			x = -166,
+			y = 50,
+			shape = {0,1, 52,1, 52,30, 0,30},
+			sprite = "assets/platforms/default-small-1.png"
+		},
+		{
+			x = -17,
+			y = -50,
+			shape = {0,1, 34,1, 34,16, 0,16},
+			sprite = "assets/platforms/default-small-1.png"
+		}
+	},
+	decorations = {}
+}
